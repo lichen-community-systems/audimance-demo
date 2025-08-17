eaf04423bd159f8216e37e53b0725f716053258c
Colin Clark <colinbdclark@gmail.com>
2025-08-17 15:46:18 -0400

    Resolves gh-1: Updates the demo to use Wired instead of Descent.

--- dev.sh
+++ dev.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+set -e
 
 pushd ../audimance
+if ! command -v nvm >/dev/null 2>&1; then
+    if [ -s "$HOME/.nvm/nvm.sh" ]; then
+        source "$HOME/.nvm/nvm.sh"
+    fi
+
+    nvm use
+fi
+
 npm run build
 go generate
 go build
 go install
 popd
+
 ~/go/bin/audimance -debug
